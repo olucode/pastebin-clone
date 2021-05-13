53f3419725bdea6de69c8463c1cd341f398400a0
Olumide Falomo <olumightyfalomo@gmail.com>
2021-05-13 02:42:21 +0100

    test: Fix e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable jest/expect-expect */
 
 import { INestApplication } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
 
 import { AppModule } from '../src/app.module';
+
+import { MockAuthGuard } from 'src/utils/mocks/shared.mocks';
+import { SampleUserRow } from 'src/utils/mocks/users.mocks';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -14,18 +19,17 @@ describe('AppController (e2e)', () => {
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideGuard(AuthGuard())
+      .useValue(MockAuthGuard)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
   it('POST /api/auth/register', async () => {
-    const input = {
-      name: 'a',
-      email: 'a@a.com',
-      password: '12345',
-    };
+    const { id: _, ...input } = SampleUserRow;
 
     await request(app.getHttpServer())
       .post('/api/auth/register')
@@ -34,10 +38,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('POST /api/auth/login', async () => {
-    const input = {
-      email: 'a@a.com',
-      password: '12345',
-    };
+    const { id: _, name: __, ...input } = SampleUserRow;
 
     const res = await request(app.getHttpServer())
       .post('/api/auth/login')
@@ -51,16 +52,12 @@ describe('AppController (e2e)', () => {
       .get('/api/users')
       .set('Authorization', `Bearer ${token}`)
       .expect(200);
-    expect(users.body[0].id).toBe(1);
-    expect(users.body[0].name).toBe('a');
+
+    expect(users.body[0].email).toBe(SampleUserRow.email);
+    expect(users.body[0].name).toBe(SampleUserRow.name);
   });
 
-  it('GET /api/users/profile', async () => {
-    const user = await request(app.getHttpServer())
-      .get('/api/users/a')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200);
-
-    expect(user.body.name).toBe('a');
+  afterAll(async () => {
+    await app.close();
   });
 });
